002bbe7cb2abe36c93f7bca60b8eafca8b1af4f9
Gusakov Kiril <gusakov24@gmail.com>
2022-07-14 15:01:39 +0300

    class ServerSocket и консольный messenger

---
+++ src/com/lirik/socket/messenger/Client.java
@@ -0,0 +1,34 @@
+package com.lirik.socket.messenger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * После запуска сервера можно запускать клиент и печатать сообщения!!!
+ * Когда отправим на сервер слово stop, то сервер остановит свое выполнение и программа остановится, а у клиента вывалится EXCEPTION!!!
+ */
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        InetAddress inetAddress = Inet4Address.getByName("localhost");
+
+
+        try (Socket socket = new Socket(inetAddress, 7777);
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());   //  необходим для отправки запроса на сервер
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {    // необходим для считывания ответа от сервера
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                outputStream.writeUTF(request); // отправили запрос на сервер
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
+        }
+    }
+}
---
+++ src/com/lirik/socket/messenger/Server.java
@@ -0,0 +1,33 @@
+package com.lirik.socket.messenger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Сначала стартуем сервер!!1
+ */
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket serverSocket = new ServerSocket(7777);
+             Socket socket = serverSocket.accept();
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream()); // ответ для клиента, формируемый сервером
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {  // запрос, пришедший от клиента
+            Scanner scanner = new Scanner(System.in);
+            String request = inputStream.readUTF();
+            while (!"stop".equals(request)) {
+                System.out.println("Request from client: " + request);
+                String response = scanner.nextLine();
+                outputStream.writeUTF(response);
+                request = inputStream.readUTF();
+            }
+        }
+    }
+
+}
